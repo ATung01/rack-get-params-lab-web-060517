c2e13eb73372329651fc08248757be36a61b182f
github username <github email address>
2017-06-26 13:11:47 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,8 @@
+require 'pry'
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -9,6 +11,20 @@ class Application
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
+      end
+    elsif req.path.match(/cart/)
+      if @@cart != []
+        @@cart.each { |cart_item| resp.write cart_item + "\n"}
+      else
+        resp.write "Your cart is empty"
+      end
+    elsif req.path.match(/add/)
+      cart_item = req.params["item"]
+      if @@items.include?(cart_item)
+        @@cart << cart_item
+        resp.write "added #{cart_item}"
+      else
+        resp.write "We don't have that item"
       end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
